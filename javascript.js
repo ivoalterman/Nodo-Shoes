4be5b012c71812fdc8c18bee7be9d126c1f0c12f
ivoalterman <138502616+ivoalterman@users.noreply.github.com>
2023-07-06 12:54:41 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let mostrarzapatos = () => {
+    alert("Entra")
+//let zapato1 = document.getElementById("Borcegos").checked;
+console.log(zapato1)
+
+
+document.getElementById("img_borcego").style.display="block";
+document.getElementById("zapatos_mujer").style.display="none";
+}
